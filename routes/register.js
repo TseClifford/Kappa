6fcfe61bb4e278577113e048f2c942817258ec0a
Clifford Tse <TseClifford@gmail.com>
2022-02-14 05:21:16 +0000

    add register check existing email

--- routes/register.js
+++ routes/register.js
@@ -11,16 +11,16 @@ module.exports = (db) => {
 
     .post("/", (req, res) => {
       db.query(`SELECT * FROM users WHERE email = '${req.body.email}';`)
-        // .then((data) => {
-        //   if (data.length > 0) {
-        //     throw new Error('This email is already in use.')
-        //   }
-        // })
-        .then(() => {
-          db.query(`INSERT INTO users (name, email, password) VALUES ('${req.body.name}', '${req.body.email}', '${bcrypt.hashSync(req.body.password, salt)}');`);
-          req.session["email"] = req.body.email;
-          res.redirect('/');
+        .then((data) => {
+          if (data.rows.length > 0) {
+            res.send('This email has already been used.');
+          } else {
+            db.query(`INSERT INTO users (name, email, password) VALUES ('${req.body.name}', '${req.body.email}', '${bcrypt.hashSync(req.body.password, salt)}');`);
+            req.session["email"] = req.body.email;
+            res.redirect('/');
+          }
         })
+
         .catch((err) => {
           res.status(500).json({ error: err.message });
         });
