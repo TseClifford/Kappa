98a9bdafb2b55481d7170d950a3aa17710f37c0f
Clifford Tse <TseClifford@gmail.com>
2022-02-15 23:05:16 +0000

    update user seeds emails to lowercase

--- db/seeds/01_users_seed.sql
+++ db/seeds/01_users_seed.sql
@@ -1,5 +1,5 @@
-INSERT INTO users (name, email, password, admin) VALUES ('Alice', 'Alice@gmail.com', '$2a$10$vZWmQSyMnkg7/Faj73QWTuSXiqLjsTVgR5yzL5ZmC9KPQFtoxyvCO', 'true');
-INSERT INTO users (name, email, password, admin) VALUES ('Kira', 'Kira@gmail.com', '$2a$10$vZWmQSyMnkg7/Faj73QWTuSXiqLjsTVgR5yzL5ZmC9KPQFtoxyvCO', 'true');
-INSERT INTO users (name, email, password, admin) VALUES ('Bob', 'Bob@gmail.com', '$2a$10$vZWmQSyMnkg7/Faj73QWTuSXiqLjsTVgR5yzL5ZmC9KPQFtoxyvCO', 'true');
-INSERT INTO users (name, email, password, admin) VALUES ('John', 'John@gmail.com', '$2a$10$vZWmQSyMnkg7/Faj73QWTuSXiqLjsTVgR5yzL5ZmC9KPQFtoxyvCO', 'false');
-INSERT INTO users (name, email, password, admin) VALUES ('Rob', 'Rob@gmail.com', '$2a$10$vZWmQSyMnkg7/Faj73QWTuSXiqLjsTVgR5yzL5ZmC9KPQFtoxyvCO', 'false');
+INSERT INTO users (name, email, password, admin) VALUES ('Alice', 'alice@gmail.com', '$2a$10$vZWmQSyMnkg7/Faj73QWTuSXiqLjsTVgR5yzL5ZmC9KPQFtoxyvCO', 'true');
+INSERT INTO users (name, email, password, admin) VALUES ('Kira', 'kira@gmail.com', '$2a$10$vZWmQSyMnkg7/Faj73QWTuSXiqLjsTVgR5yzL5ZmC9KPQFtoxyvCO', 'true');
+INSERT INTO users (name, email, password, admin) VALUES ('Bob', 'bob@gmail.com', '$2a$10$vZWmQSyMnkg7/Faj73QWTuSXiqLjsTVgR5yzL5ZmC9KPQFtoxyvCO', 'true');
+INSERT INTO users (name, email, password, admin) VALUES ('John', 'john@gmail.com', '$2a$10$vZWmQSyMnkg7/Faj73QWTuSXiqLjsTVgR5yzL5ZmC9KPQFtoxyvCO', 'false');
+INSERT INTO users (name, email, password, admin) VALUES ('Rob', 'rob@gmail.com', '$2a$10$vZWmQSyMnkg7/Faj73QWTuSXiqLjsTVgR5yzL5ZmC9KPQFtoxyvCO', 'false');
